714e0f01ebaa2b2fb00d4dabe619d9e725508aeb
trung-kieen <trungkiennk4869@gmail.com>
2025-03-01 14:51:57 +0700

    feat: dynamic header

--- clockee-ui/src/app/components/Header/Header.tsx
+++ clockee-ui/src/app/components/Header/Header.tsx
@@ -1,9 +1,58 @@
-export const Header = () => { 
+'use client';
+import { ShoppingCart, User } from "lucide-react"
+import Brand from "../Brand"
+import { usePathname } from 'next/navigation'
+
+
+const NavLinks = () => {
+
   return (
-    <div className="bg-white-800 text-white p-4 w-full h-[81px] flex items-center justify-between">
-      <div className="ml-[20px]">
-        <img src="/logo_header.png" alt="" />
+    <div className="flex space-x-6 text-gray-700 font-medium">
+      {/* replace with Link */}
+      <a href="#" className="hover:text-black">THƯƠNG HIỆU</a>
+      <a href="#" className="hover:text-black">NAM</a>
+      <a href="#" className="hover:text-black">NỮ</a>
+      <a href="#" className="hover:text-black">LIÊN HỆ</a>
+    </div>
+  )
+}
+const SearchBar = () => {
+  return <input placeholder="Tìm kiếm sản phẩm" className="w-64 pr-10" />
+}
+
+
+const RightNavIcons = () => {
+  return (
+    <div className="flex items-center space-x-4">
+      <ShoppingCart className="w-6 h-6 text-gray-700 hover:text-black" />
+      <User className="w-6 h-6 text-gray-700 hover:text-black" />
+    </div>
+  )
+}
+
+const NavBar = () => {
+  const pathname = usePathname();
+  const simpleIconHeaderPages = ["/login", "logout"];
+  const showHeaderDetail = !(simpleIconHeaderPages.indexOf(pathname) > -1);
+  return (
+    <nav className="flex items-center justify-between px-6 py-4 bg-white shadow-md">
+      <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-2">
+          <Brand />
+        </div>
+        {showHeaderDetail && <NavLinks />}
       </div>
+      {showHeaderDetail && <SearchBar />}
+      {showHeaderDetail && <RightNavIcons />}
+    </nav>
+  )
+}
+
+export const Header = () => {
+
+  return (
+    <div className="bg-white-800 text-white w-full  ">
+      <NavBar />
     </div>
   )
 }
--- clockee-ui/src/app/page.tsx
+++ clockee-ui/src/app/page.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { Header } from './components/Header/Header';
-import { Footer } from './components/Footer/Footer';
 
 
-
-
-export default function Home() {
+export default function HomePage() {
 
   return (
     <>
-      <div className='bg-[#FFDA3E] '>
-        <div className='p-[120px] flex item-center justify-center'>
-          <img src="/body.png" />
-          Home page
-        </div>
-      </div>
+      Hello home page
     </>
   )
 }
