3611031d1168493468f63b445d1b47ed634fd814
cownon <duongnkt.t2.1922@gmail.com>
2025-03-19 21:13:46 +0700

    No merge to dev

--- clockee-server/src/main/java/com/example/clockee_server/controller/admin/AdminProductController.java
+++ clockee-server/src/main/java/com/example/clockee_server/controller/admin/AdminProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin/products")
+@RequestMapping("api/admin/products")
 public class AdminProductController {
 
     @Autowired
