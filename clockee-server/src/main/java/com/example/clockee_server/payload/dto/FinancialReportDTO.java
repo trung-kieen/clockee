23e5f64cee5240967f3599b453c49f6af23cf103
cownon <duongnkt.t2.1922@gmail.com>
2025-04-13 21:01:42 +0700

    add financial report

---
+++ clockee-server/src/main/java/com/example/clockee_server/payload/dto/FinancialReportDTO.java
@@ -0,0 +1,14 @@
+package com.example.clockee_server.payload.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FinancialReportDTO {
+    private Double totalSale;
+    private Double totalPurchase;
+    private Double profit;
+}
