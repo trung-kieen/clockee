77c04f04006787b506beb19e1ede0cea27efafac
trung-kieen <trungkiennk4869@gmail.com>
2025-05-08 22:39:11 +0700

    ci-cd: docker build for server and client with ci-cd pipeline

--- clockee-server/build-image.sh
+++ clockee-server/build-image.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 mvn clean package
 
-docker build -t clockee-server:latest . && docker run -p 8080:8080 clockee-server
+docker build -t clockee-server:latest . && docker run --network clockee_clockee-network -p 8080:8080 -e DBMS_HOST=clockee-db clockee-server
