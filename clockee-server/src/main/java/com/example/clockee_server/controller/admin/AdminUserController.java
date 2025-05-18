b0f036283742e9cf7f4d734e2c5975c068c42eab
Nguyen Duc Vi <nducvi.information@gmail.com>
2025-05-18 21:31:51 +0700

    reformat AdminUserService follows Google Java Style Guide

--- clockee-server/src/main/java/com/example/clockee_server/controller/admin/AdminUserController.java
+++ clockee-server/src/main/java/com/example/clockee_server/controller/admin/AdminUserController.java
@@ -1,5 +1,23 @@
+// package com.example.clockee_server.controller.admin;
+
+
+// import com.example.clockee_server.config.ApplicationConstants;
+// import com.example.clockee_server.entity.Role;
+// import com.example.clockee_server.payload.PageResponse;
+// import com.example.clockee_server.payload.dto.UserDetailResponse;
+// import com.example.clockee_server.service.AdminUserService;
+
+// import java.util.Set;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
+
+// import org.springframework.data.domain.Page;
+
 package com.example.clockee_server.controller.admin;
 
+import java.util.Set;
 
 import com.example.clockee_server.config.ApplicationConstants;
 import com.example.clockee_server.entity.Role;
@@ -7,14 +25,10 @@ import com.example.clockee_server.payload.PageResponse;
 import com.example.clockee_server.payload.dto.UserDetailResponse;
 import com.example.clockee_server.service.AdminUserService;
 
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import org.springframework.data.domain.Page;
-
 
 
 @RestController
@@ -22,10 +36,10 @@ import org.springframework.data.domain.Page;
 public class AdminUserController {
     @Autowired private AdminUserService adminUserService;
 
-    // Lấy hết user có phân trang chưa có điều kiện specification
+    // get all user with paging and no specification
     @GetMapping
-    ResponseEntity<PageResponse<UserDetailResponse>> getAllUsers(
-            @RequestParam (defaultValue = ApplicationConstants.PAGE_NUMBER) int page,
+    public ResponseEntity<PageResponse<UserDetailResponse>> getAllUsers(
+            @RequestParam(defaultValue = ApplicationConstants.PAGE_NUMBER) int page,
             @RequestParam(defaultValue = ApplicationConstants.PAGE_SIZE) int size
     ){
         Page<UserDetailResponse> users = adminUserService.getAllUsers(page, size);
@@ -33,9 +47,9 @@ public class AdminUserController {
         return ResponseEntity.ok(new PageResponse<>(users));
     }
 
-    // this function is to get user with id nè
+    // this function is to get user with id
     @GetMapping("/{id}")
-    ResponseEntity<UserDetailResponse> getUserById(@PathVariable Long id){
+    public ResponseEntity<UserDetailResponse> getUserById(@PathVariable Long id){
         UserDetailResponse user = adminUserService.getUserById(id);
         return ResponseEntity.ok(user);
     }
@@ -43,13 +57,13 @@ public class AdminUserController {
 
     // this function is to get the user's role id, name, authorities timtimm
     @GetMapping("/{id}/roles")
-    ResponseEntity<Set<Role>> getRoleById(@PathVariable Long id){
+    public ResponseEntity<Set<Role>> getRoleById(@PathVariable Long id){
         Set<Role> roles = adminUserService.getRolesByUserId(id);
         return ResponseEntity.ok(roles);
     }
     
     @PatchMapping("/{id}/deleted")
-    public ResponseEntity<String> updateUserdetedStatus(
+    public ResponseEntity<String> updateUserdeletedStatus(
         @PathVariable Long id,
         @RequestParam("isDeleted") Boolean isDeleted){
             adminUserService.upDateDeletedStatus(id, isDeleted);
